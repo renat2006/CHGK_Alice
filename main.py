f9b264a25432f06156cda7d31027b2228ef0005d
Renat <testinggame778@gmail.com>
2023-05-06 21:03:45 +0300

    v 1.0.1

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import logging
+import os
 import random
 import psycopg2
 from aioalice.utils.helper import Helper, HelperMode, Item
 from aiohttp import web
 from aioalice import Dispatcher, get_new_configured_app, types
 from aioalice.dispatcher import MemoryStorage
-
+from dotenv import load_dotenv
+load_dotenv()
 WEBHOOK_URL_PATH = '/my-alice-webhook/'  # webhook endpoint
 
 WEBAPP_HOST = 'localhost'
 WEBAPP_PORT = 3001
-SKILL_ID = 'e6769ffe-cc98-4375-bab9-c992027649b9'
-OAUTH_TOKEN = 'y0_AgAAAAAjiGoWAAT7owAAAADe4ez8G_br3guXSv2t4CeYQLObwUmlieo'
+SKILL_ID = os.getenv("SKILL_ID")
+OAUTH_TOKEN = os.getenv("OAUTH_TOKEN")
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
@@ -19,9 +21,7 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
 # Создаем экземпляр диспетчера и подключаем хранилище в памяти
 dp = Dispatcher(storage=MemoryStorage(), skill_id=SKILL_ID, oauth_token=OAUTH_TOKEN)
 
-def connect_to_db():
-    import psycopg2
-
+def get_random_question():
     conn = psycopg2.connect("""
         host=rc1b-1dkhcvps79tr5wu2.mdb.yandexcloud.net
         port=6432
@@ -31,12 +31,20 @@ def connect_to_db():
         target_session_attrs=read-write
     """)
 
-    q = conn.cursor()
-    q.execute('SELECT version()')
+    cur = conn.cursor()
 
-    print(q.fetchone())
 
+    cur.execute("SELECT * FROM questions ORDER BY random() LIMIT 1")
+
+
+    result = cur.fetchone()
+    random_string = result[0] if result else ''
+
+    cur.close()
     conn.close()
+    print("ok")
+    return random_string
+
 class GameStates(Helper):
     mode = HelperMode.snake_case
 
@@ -47,7 +55,8 @@ class GameStates(Helper):
 @dp.request_handler(func=lambda areq: areq.session.new)
 async def handle_new_session(alice_request):
     print(alice_request)
-    connect_to_db()
+
+    print(get_random_question())
     return alice_request.response('Привет! Купи слона!', buttons=["Прив"])
 
 if __name__ == '__main__':
